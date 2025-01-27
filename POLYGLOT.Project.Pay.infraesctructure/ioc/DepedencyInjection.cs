3553678b582a73c779733525490768f9060fdacc
Juan Diego Benavides <jdbenabides@intuitosa.com>
2025-01-27 16:48:01 -0500

    Feature: Inicializar Db Context

--- POLYGLOT.Project.Pay.infraesctructure/ioc/DepedencyInjection.cs
+++ POLYGLOT.Project.Pay.infraesctructure/ioc/DepedencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace POLYGLOT.Project.Pay.infraestructure.ioc
@@ -7,6 +8,7 @@ namespace POLYGLOT.Project.Pay.infraestructure.ioc
     {
         public static IServiceCollection AddInfraestructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<DbContext>(opt => opt.UseMySQL(configuration.GetConnectionString("DefaultConnection")));
             return services;
         }
     }
