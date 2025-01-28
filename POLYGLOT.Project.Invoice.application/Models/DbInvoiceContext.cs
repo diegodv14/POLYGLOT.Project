123a7350ff3d8790983ae75c747db61cc186f921
Juan Diego Benavides <jdbenabides@intuitosa.com>
2025-01-28 12:53:37 -0500

    Feature: Scaffold Invoice Micro

---
+++ POLYGLOT.Project.Invoice.application/Models/DbInvoiceContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace POLYGLOT.Project.Invoice.application.Models;
+
+public partial class DbInvoiceContext : DbContext
+{
+    public DbInvoiceContext()
+    {
+    }
+
+    public DbInvoiceContext(DbContextOptions<DbInvoiceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Invoice> Invoices { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=db_invoice;Username=invoicePrueba;Password=prueba;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Invoice>(entity =>
+        {
+            entity.HasKey(e => e.IdInvoice).HasName("invoices_pkey");
+
+            entity.ToTable("invoices");
+
+            entity.Property(e => e.IdInvoice)
+                .ValueGeneratedNever()
+                .HasColumnName("idInvoice");
+            entity.Property(e => e.Amount).HasColumnName("amount");
+            entity.Property(e => e.State)
+                .HasDefaultValueSql("NULL::\"bit\"")
+                .HasColumnType("bit(1)")
+                .HasColumnName("state");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ POLYGLOT.Project.Invoice.application/Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace POLYGLOT.Project.Invoice.application.Models;
+
+public partial class Invoice
+{
+    public int IdInvoice { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public BitArray? State { get; set; }
+}
